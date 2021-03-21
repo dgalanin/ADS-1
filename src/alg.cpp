cec8ed022512dbf5a403bc07c83ebd09a1ad3d0d
Dmitry <71137816+dgalanin@users.noreply.github.com>
2021-03-21 17:12:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int cbinsearch(int *arr, int size, int value) {
     int low = 0, high = size - 1, left = -1, right = -1;
     int resLeft = -1, resRight = -1;
     while (low <= high) {
